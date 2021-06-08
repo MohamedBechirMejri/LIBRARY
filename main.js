0de1006bea1dab239e3719cac16d55fc460357af
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-06-08 14:39:11 +0100

    add new book to page

--- main.js
+++ main.js
@@ -25,7 +25,43 @@ const totalPages = document.getElementById("number-of-pages");
 const readPages = document.getElementById("pages-read");
 const submitButton = document.getElementById("submit-button");
 
+const booksSection = document.getElementById("books-section");
+const div = document.createElement("div");
+
 // funcs
+
+function updateLibrary() {
+  myLibrary.forEach((book) => {
+    div.classList.add("book");
+    div.innerHTML = `<img
+          src="https://i.pinimg.com/originals/7f/3c/fa/7f3cfa1073a5484fc6899f4c47e008fb.jpg"
+          alt="harry potter cover"
+        />
+        <h2>
+          <span class="booktitle"
+            >${book.title}</span
+          >
+          <br />
+          by <br />
+          <span class="bookauthor">${book.author}</span>
+        </h2>
+
+        <div class="pages-read">
+          <button class="button">+</button>
+          <h5>
+            <p class="pages-read">${readPages}</p>
+            /
+            <p class="total">${totalPages}</p>
+          </h5>
+          <button class="button">-</button>
+        </div>
+        <div class="comp-rem-buttons">
+          <button class="completed button is-primary">completed</button>
+          <button class="remove button is-danger">remove</button>
+        </div>`;
+    booksSection.appendChild(div);
+  });
+}
 
 submitButton.addEventListener("click", () => {
   addBookToLibrary(
@@ -38,4 +74,5 @@ submitButton.addEventListener("click", () => {
   authorInput.value = "";
   totalPages.value = "";
   readPages.value = "";
+  updateLibrary();
 });
