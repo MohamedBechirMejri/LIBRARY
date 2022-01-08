5d3fd72950ef54358d34716aaf482e98dfd56e67
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-08 22:25:48 +0100

    init firebase

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";  
+import { BrowserRouter } from "react-router-dom";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRmEa6CLutnmq7Zc-zLzJ7leaIuN32WBI",
+  authDomain: "library-top-a7458.firebaseapp.com",
+  projectId: "library-top-a7458",
+  storageBucket: "library-top-a7458.appspot.com",
+  messagingSenderId: "911226944155",
+  appId: "1:911226944155:web:628fae2e3bc68f6abf243e",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
