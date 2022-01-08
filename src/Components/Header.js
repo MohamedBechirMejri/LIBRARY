61d10e5992b5c8f9095ecffac5d9306d0e3f363a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-08 09:04:32 +0100

    tweak header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,15 @@
 const Header = (props) => {
   return (
-    <header
-      className={`" fixed  w-screen ${
-        props.isAddingBook ? "h-screen" : "h-[5em] "
-      }  text-white bg-black  uppercase font-extrabold font-serif  transition-all duration-500 ease-in-out flex flex-col items-center  justify-center "`}
-    >
-      <div className="flex items-center justify-center  gap-[4em]">
+    <>
+      <header className=" w-screen h-[5em] text-white bg-[#009261] uppercase font-extrabold font-serif flex gap-[3em] items-center justify-center ">
         <div className="w-[2em] bg-transparent h-[2em]" />
-        <h1 className="m-0 text-xl">Library</h1>
+        <h1 className="m-0 text-4xl">Library</h1>
         <div
           className="w-[2em] bg-white h-[2em]"
           onClick={() => props.setIsAddingBook(!props.isAddingBook)}
         />
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
