26bb43eff3b12b7ee957d7056e1b3ff2b64a7315
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-08 10:02:40 +0100

    tweak header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 const Header = (props) => {
   return (
     <>
-      <header className=" w-screen h-[5em] text-white bg-[#009261] uppercase font-extrabold font-serif flex gap-[3em] items-center justify-center ">
+      <header className=" w-screen h-[5em] text-white bg-[#00d1b2] uppercase font-extrabold font-serif flex gap-[3em] items-center justify-center z-10 ">
         <div className="w-[2em] bg-transparent h-[2em]" />
         <h1 className="m-0 text-4xl">Library</h1>
         <div
