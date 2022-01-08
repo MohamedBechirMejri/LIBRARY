ddc0b8561a52ecdd5361ffecd1a27a8bd971ecbe
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-08 07:39:04 +0100

    style header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,18 @@
 const Header = (props) => {
   return (
-    <header className="fixed flex justify-center w-screen h-[5em] text-white bg-black items-center uppercase font-extrabold font-serif gap-[3em]">
-      <div className="w-[2em] bg-white h-[2em]" />
-      <h1 className="m-0 text-xl">Library</h1>
-      <div
-        className="w-[2em] bg-white h-[2em]"
-        onClick={() => props.setIsAddingBook(!props.isAddingBook)}
-      />
+    <header
+      className={`" fixed  w-screen ${
+        props.isAddingBook ? "h-screen" : "h-[5em] "
+      }  text-white bg-black  uppercase font-extrabold font-serif  transition-all duration-500 ease-in-out flex flex-col items-center  justify-center "`}
+    >
+      <div className="flex items-center justify-center  gap-[4em]">
+        <div className="w-[2em] bg-transparent h-[2em]" />
+        <h1 className="m-0 text-xl">Library</h1>
+        <div
+          className="w-[2em] bg-white h-[2em]"
+          onClick={() => props.setIsAddingBook(!props.isAddingBook)}
+        />
+      </div>
     </header>
   );
 };
