33a63ea8ab0202851c8d890c3acc73b07e8b7028
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-10 05:58:17 +0100

    animate loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddBook from "./Components/AddBook";
 import Header from "./Components/Header";
 import Books from "./Components/Books";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithRedirect,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
-const App = () => {
+const App = (props) => {
+  const auth = getAuth();
+  const db = getFirestore();
+  const logOut = () => {
+    auth.signOut();
+  };
+  const signIn = () => {
+    setIsLoading(true);
+    signInWithRedirect(auth, new GoogleAuthProvider());
+  };
+
   const [isAddingBook, setIsAddingBook] = useState(false);
+  const [user, setUser] = useState(null);
   const [books, setBooks] = useState([
     {
       id: 1,
@@ -47,18 +66,57 @@ const App = () => {
       isFinished: false,
     },
   ]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  return (
-    <div className="flex flex-col items-center justify-start min-h-screen">
-      <Header isAddingBook={isAddingBook} setIsAddingBook={setIsAddingBook} />
-      {isAddingBook && (
-        <AddBook
-          books={books}
-          setBooks={setBooks}
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4442000);
+  }, []);
+
+  onAuthStateChanged(auth, (user) => {
+    setUser(user);
+    console.log(user);
+  });
+  return isLoading ? (
+    <h1 className=" font-extrabold uppercase animate-bouncee text-[3em] text-[#00d1b2] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      Loading...
+    </h1>
+  ) : (
+    <div
+      className="flex flex-col items-center justify-start min-h-screen"
+      onClick={() => console.log(auth)}
+    >
+      {user && (
+        <Header
+          isAddingBook={isAddingBook}
           setIsAddingBook={setIsAddingBook}
+          logOut={logOut}
         />
       )}
-      {!isAddingBook && <Books books={books} setBooks={setBooks} />}
+      {user ? (
+        isAddingBook ? (
+          <AddBook
+            books={books}
+            setBooks={setBooks}
+            setIsAddingBook={setIsAddingBook}
+          />
+        ) : (
+          <Books books={books} setBooks={setBooks} />
+        )
+      ) : (
+        <div className="fixed inset-0 flex flex-col items-center justify-center gap-4   text-[#00d1b2]">
+          <h1 className="text-4xl font-extrabold uppercase">
+            Welcome to your personal Library
+          </h1>
+          <button
+            className="border-[#00d1b2] rounded border-2 text-[#00d1b2] hover:bg-[#00d1b2] hover:text-white duration-500 px-6 py-1 active:scale-95  font-extrabold w-[12em] h-[4em] "
+            onClick={signIn}
+          >
+            Sign In!
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,23 @@ module.exports = {
     extend: {
       animation: {
         reveal: "reveal 0.5s ease-in-out",
+        bouncee: "bouncee 1s infinite",
       },
       keyframes: {
         reveal: {
           "0%": {
             opacity: 0,
             transform: "translate(0, 10px)",
+          },
+        },
+        bouncee: {
+          "0%, 100%": {
+            transform: "translateX(-50%) translateY(-55%)",
+            "animation-timing-function": "cubic-bezier(0.8,0,1,1);",
+          },
+          "50%": {
+            transform: "translateX(-50%)",
+            "animation-timing-function": "cubic-bezier(0,0,0.2,1);",
           },
         },
       },
